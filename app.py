6dfbdd2d374fc1317e7c190414e36d761f2abeaa
Guilherme Denardi <gdenardi@outlook.com>
2024-04-13 10:15:34 -0300

    creating class Tasks and creating method POST for create task

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from models.task import Task
 
-# __name__ = "__main__"
 app = Flask(__name__)
 
+#CRUD
+#Create, Read, Update, Delete
 
-#Criando as primeiras rotas
-@app.route("/")
+tasks = []
+task_id_control = 1
 
-def hello_world():
-    return "Hello World!"
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    global task_id_control
+    data = request.get_json()
+    new_task = Task(id=task_id_control, title=data['title'], description=data.get('description', ''))
+    task_id_control += 1
+    tasks.append(new_task)
+    print(tasks)
+    return jsonify({"message": "Nova tarefa criada com sucesso!"})
 
-@app.route("/about")
-def about():
-    return "Página sobre"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
